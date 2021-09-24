711a3a52bd9023e0e3922e83eb5d3fd50e064f6d
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-24 15:46:07 +0530

    [Chaitanya] Add : Added Vending machine problem

---
+++ NUnitTesting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace NUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // run or exit
+            bool isRun = true;
+            while (isRun)
+            {
+                // Select which program to run
+                Console.WriteLine("\n Select a program number from 1 to 7 only to run selected program or select 8 to exit application:\n" +
+                    "1. Vending Machine, 2. Day of week, 3. Temperature Conversion, 4. Monthly Payment, \n" +
+                    "5. Sqare root, 6. Convert to Binary, 7. Nibbles swapped Binary to decimal, 8. Exit application");
+                int selectProgram = Convert.ToInt32(Console.ReadLine());
+                switch (selectProgram)
+                {
+                    case 1:
+                        new VendingMachine().GetChangeByIter();
+                        new VendingMachine().GetChangeByRecur();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    default:
+                        isRun = !isRun;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ NUnitTesting/VendingMachine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTesting
+{
+    class VendingMachine
+    {
+        public int Change()
+        {
+            // get the required change
+            Console.WriteLine("Enter the amount of change you need to get, in Rs.: ");
+            int changeToGet = Convert.ToInt32(Console.ReadLine());
+            while (changeToGet <= 0)
+            {
+                Console.WriteLine("Change should be greater than 0!... ");
+                Console.WriteLine("Enter the correct correct amount of change you need to get, in Rs.: ");
+                changeToGet = Convert.ToInt32(Console.ReadLine());
+            }
+            return changeToGet;
+        }
+        // initialize constant
+        int[] NOTES_LIST = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+
+        // initialize variables
+        int notesCount = 0;
+        // int changeToGet = new VendingMachine().Change();
+
+        public void GetChangeByIter()
+        {
+            int changeToGet = new VendingMachine().Change();
+            for (int i = 0; i < NOTES_LIST.Length; i++)
+            {
+                int note = NOTES_LIST[i];
+                while (changeToGet / note != 0)
+                {
+                    notesCount += 1;
+                    changeToGet -= note;
+                }
+                Console.WriteLine($"Number of {note} Rs. notes are: {notesCount}");
+                notesCount = 0;
+            }
+        }
+        // method to run by recursion
+        public int NumberOfNotes(int changeToGet, int note)
+        {
+            while (changeToGet / note != 0)
+            {
+                notesCount += 1;
+                changeToGet -= note;
+            }
+            Console.WriteLine($"Number of {note} Rs. notes are: {notesCount}");
+            notesCount = 0;
+            return changeToGet;
+        }
+        public void GetChangeByRecur()
+        {
+            int changeToGet = new VendingMachine().Change();
+            foreach (int note in NOTES_LIST)
+            {
+                changeToGet = new VendingMachine().NumberOfNotes(changeToGet, note);
+            }
+        }
+    }
+}
